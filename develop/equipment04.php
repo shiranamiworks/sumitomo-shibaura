f2d038eae6e80a96e0fcba0c48b979c14a1378fb
福井彩佳 <ayaka.fsp@gmail.com>
2025-04-14 17:38:39 +0900

    no message

--- develop/equipment04.php
+++ develop/equipment04.php
@@ -44,7 +44,7 @@
           <p class="tit-img"><span>荷物が多いときや、車椅子利用の方にも便利な</span>ダブルオートドア</p>
           <p class="lead">風除室 ・1階エレベーターホールの入口には、それぞれオートドアを採用しました。オートロックシステムのハンズフリーキーと合わせることで車椅子での行き来や、荷物を抱えての通行もスムーズに行うことができます。</p>
           <div class="image">
-            <img src="./images/equipment04/img01.jpg" alt="" class="">
+            <img src="./images/equipment04/img01.png" alt="" class="">
             <p class="annotation gothic">（概念図）</p>
           </div>
         </li>
@@ -52,8 +52,8 @@
           <p class="tit-img"><span>鍵を取り出すことなく解錠可能な</span>ハンズフリーキー</p>
           <p class="lead">主なオートロックドアには、鍵をカバンやポケットに入れたままで解錠可能な、ハンズフリーキーを採用。<span>※周辺通行時の意図しない解錠・誤作動防止のため、設置箇所により認証方法・検知範囲が異なります。また、検知範囲には限度があり、構造上、動線がカバーされていない箇所もあります。</span></p>
           <div class="image">
-            <img src="./images/equipment04/img02.jpg" alt="" class="">
-            <p class="annotation left gothic">（概念図）</p><p class="annotation gothic">参考写真1</p>
+            <img src="./images/equipment04/img02.png" alt="" class="">
+            <p class="annotation left gothic">（概念図）</p><p class="annotation gothic">参考写真</p>
           </div>
         </li>
         <li>
@@ -90,7 +90,7 @@
           <p class="tit-img mt30"><span>ライフスタイルに合わせて間取りを変更できる</span>ダブルオープンフレキシブルプラン</p>
           <p class="lead">リビング・ダイニングと隣接する洋室を可動式間仕切り扉で開放的な一体空間にもプライバシーの保たれた個室にも変更可能なダブルオープンフレキシブルプランを採用しました。主開口とは反対側からも単独で開くことができる扉を採用することで洋室のプライベート性や家具レイアウトの自由度が大きくアップしました。ライフスタイルの変化にリフォームすることなく自在に対応できます。<span>※N・O・P・Qタイプを除く。</span></p>
           <div class="image">
-            <img src="./images/equipment04/img06.jpg" alt="" class="pc"><img src="./images/equipment04/img06-sp.jpg" alt="" class="tb">
+            <img src="./images/equipment04/img06.png" alt="" class="pc"><img src="./images/equipment04/img06-sp.png" alt="" class="tb">
             <p class="annotation left left03 gothic">（概念図）</p>
           </div>
         </li>
@@ -98,20 +98,20 @@
           <p class="tit-img"><span>床近くから天井近くまで、壁一面に窓を設けた</span>ダイナミックパノラマウインドウ</p>
           <p class="lead">リビング・ダイニングの開口部に、床近くから天井近くまで壁一面に窓を設けたダイナミックパノラマウインドウを採用しました。<span>※A・C・E・G・H1・H2・J・L・M・N・O・P・Qタイプのみ。</span></p>
           <div class="image">
-            <img src="./images/equipment04/img08.jpg" alt="" class="pc"><img src="./images/equipment04/img08-sp.jpg" alt="" class="tb"><p class="annotation gothic">ダイナミックパノラマウインドウの場合<br class="pc">（概念図）</p><p class="annotation left left01 gothic">バルコニーのある開口部の場合 <br class="pc">（概念図）</p>
+            <img src="./images/equipment04/img07.png" alt="" class="pc"><img src="./images/equipment04/img07-sp.png" alt="" class="tb"><p class="annotation gothic bt-20">ダイナミックパノラマウインドウの場合<br class="pc">（概念図）</p><p class="annotation left left01 bt-20 gothic">バルコニーのある開口部の場合 <br class="pc">（概念図）</p>
           </div>
         </li>
         <li>
           <p class="tit-img"><span>遠隔操作が便利</span>無線LAN給湯リモコン</p>
           <p class="lead">専用アプリケーションを使ってお手元のスマートフォンから給湯器・床暖房などの操作や機器の状態をどこからでも確認することができます。 帰宅途中にスマートフォンからお湯はり・追い焚きをしておけば、帰ってすぐお風呂に入れます。また、寒い冬の日は床暖房を運転ONにしておけば、家に帰るころにはお部屋あたたか。</p>
           <div class="image tb">
-            <img src="./images/equipment04/img09.jpg" alt="" class=""><p class="annotation gothic">（概念図）</p>
+            <img src="./images/equipment04/img08.png" alt="" class=""><p class="annotation gothic">（概念図）</p>
           </div>
           <p class="lead"><span>※1. ご利用には予め準備が必要な事項もございます。詳細につきましては取扱説明書をご確認ください。<br>※2. 家庭用無線LANルーターは別途ご用意頂く必要がございます。<br>※3. 機器ごとによって仕様・接続方法が異なりますので、詳細につきましては取扱説明書をご確認ください。<br>※4. O・P・Qタイプの床暖房遠隔操作の対象はLDとなります。</span></p>
         </li>
         <li>
           <p class="tit-img"><span>眺望を妨げることがなく、熱にも強い</span>透明耐熱強化ガラス</p>
-          <p class="lead">一部住戸には、透明耐熱強化ガラスを採用し、眺望を妨げる網をなくすと同時に強化処理を加えた防火ガラスとなっています。また万一割れた際には破片が細かく砕けるので、安全性にも優れています。<span>※詳細は係員にお尋ねください。</span></p>
+          <p class="lead mb30">一部住戸には、透明耐熱強化ガラスを採用し、眺望を妨げる網をなくすと同時に強化処理を加えた防火ガラスとなっています。また万一割れた際には破片が細かく砕けるので、安全性にも優れています。<span>※詳細は係員にお尋ねください。</span></p>
           <p class="tit-img"><span>絵画などを飾ることができる</span>ピクチャーレール下地</p>
           <p class="lead">リビング・ダイニングの戸境壁面には、ピクチャーレール下地を設置しました。</p>
         </li>
@@ -119,36 +119,36 @@
           <p class="tit-img"><span>お住まいのフロアで24時間ゴミ出しができる</span>クリーンステーション</p>
           <p class="lead">各階フロアにゴミ置場（粗大ゴミを除く）を設けています。お住まいのフロアで24時間ゴミ出しができるので大変便利です。</p>
           <div class="image tb">
-            <img src="./images/equipment04/img010.jpg" alt=""class="tb"><p class="annotation gothic">（概念図）</p>
+            <img src="./images/equipment04/img09.png" alt=""class="tb"><p class="annotation gothic">（概念図）</p>
           </div>
         </li>
         <li>
           <p class="tit-img"><span>環境負荷の抑制やエネルギー削減に貢献する</span>電気自動車充電設備</p>
           <p class="lead">駐車場内には、電気自動車・プラグインハイブリッド車の充電に対応した設備を設置し、エコカーをより身近なものとする環境を整えました。<span>※本設備のご利用は有料となります。詳しくは係員にお尋ねください。<br>※本設備は9台あり。全て該当駐車場の契約者のみが利用可能です。</span></p>
           <div class="image tb">
-            <img src="./images/equipment04/img011.jpg" alt=""class="tb"><p class="annotation gothic">（イメージイラスト）<br>※本イラストはイメージであり、実際のものとは異なります。</p>
+            <img src="./images/equipment04/img10.png" alt=""class="tb"><p class="annotation gothic">（イメージイラスト）<br>※本イラストはイメージであり、実際のものとは異なります。</p>
           </div>
         </li>
         <li>
           <p class="tit-img"><span>効率のよいレイアウトの</span>自転車置場</p>
           <p class="lead">総戸数180戸に対して約115％、207台分の駐輪ラック付き自転車置場をご用意しました。<span>※内16台は平置となります。</span></p>
           <div class="image tb">
-            <img src="./images/equipment04/img012.jpg" alt=""class="tb"><p class="annotation gothic">（参考写真）</p>
+            <img src="./images/equipment04/img11.png" alt=""class="tb"><p class="annotation gothic">（参考写真）</p>
           </div>
         </li>
         <li>
           <div class="image pc">
-            <img src="./images/equipment04/img010.jpg" alt=""class="pc"><p class="annotation gothic">（概念図）</p>
+            <img src="./images/equipment04/img09.png" alt=""class="pc"><p class="annotation gothic">（概念図）</p>
           </div>
         </li>
         <li>
           <div class="image pc">
-            <img src="./images/equipment04/img011.jpg" alt=""class="pc"><p class="annotation gothic">（イメージイラスト）<br>※本イラストはイメージであり、実際のものとは異なります。</p>
+            <img src="./images/equipment04/img10.png" alt=""class="pc"><p class="annotation gothic">（イメージイラスト）<br>※本イラストはイメージであり、実際のものとは異なります。</p>
           </div>
         </li>
         <li>
           <div class="image pc">
-            <img src="./images/equipment04/img012.jpg" alt=""class="pc"><p class="annotation gothic">（参考写真）</p>
+            <img src="./images/equipment04/img11.png" alt=""class="pc"><p class="annotation gothic">（参考写真）</p>
           </div>
         </li>
         <li>
@@ -159,21 +159,21 @@
           <p class="tit-img"><span>点検作業等を考慮した</span>点検口</p>
           <p class="lead">配管の点検や各種メンテナンスなどの維持管理をしやすくするために各所に点検口を設けています。</p>
           <div class="image">
-            <img src="./images/equipment04/img13.jpg" alt="" class=""><p class="annotation gothic"></p>
+            <img src="./images/equipment04/img12.jpg" alt="" class=""><p class="annotation gothic"></p>
           </div>
         </li>
         <li>
-          <p class="tit-img"><span>玄関に入ると自動点灯する</span>人感センサースイッチ</p>
-          <p class="lead">玄関には、人が入るとセンサーが感知して照明が自動点灯する、人感センサースイッチを採用しました。<span><※一部住戸のセンサーが天井設置となっております。</span></p>
+          <p class="tit-img clearfix"><span>玄関に入ると自動点灯する</span>人感センサースイッチ</p>
+          <p class="lead">玄関には、人が入るとセンサーが感知して照明が自動点灯する、人感センサースイッチを採用しました。<span>※一部住戸のセンサーが天井設置となっております。</span></p>
           <div class="image">
-            <img src="./images/equipment04/img14.jpg" alt="" class=""><p class="annotation gothic"></p>
+            <img src="./images/equipment04/img13.jpg" alt="" class=""><p class="annotation gothic"></p>
           </div>
         </li>
         <li  class="half">
-          <p class="tit-img"><span>24時間365日受付</span>ライフサポートサービス</p>
+          <p class="tit-img clearfix"><span>24時間365日受付</span>ライフサポートサービス</p>
           <p class="lead">ご入居された皆様の「安心」「快適」「便利」をお手伝いさせていただくため、様々なサービスをご用意しました。</p>
           <div class="image">
-            <img src="./images/equipment04/img15.jpg" alt="" class="">
+            <img src="./images/equipment04/img14.png" alt="" class="">
           </div>
           <p class="lead"><span>※ご利用エリアにより、サービス内容が制限される場合があります。<br>また、サービス内容は変更となる場合があります。詳しくは係員にお尋ねください。</span></p>
         </li>
@@ -181,7 +181,7 @@
           <p class="tit-img"><span>お引越しをサポートする</span>お引越し無料パック</p>
           <p class="lead">梱包資材の提供や荷物の搬出・運搬・搬入など、お引越しを無料でサポートします。また、必要に応じて家電の取付けやトランクルームの手配などの各種サービスもオプション（有料）で行います。<span>※サービスのご利用には制限がございます。詳細は係員にお尋ねください。</span></p>
           <div class="image">
-            <img src="./images/equipment04/img16.jpg" alt="" class="">
+            <img src="./images/equipment04/img15.png" alt="" class="">
           </div>
         </li>
       </ul>
