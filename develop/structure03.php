2672056c89772b3bc8227cde990a3817ed69ea7f
yoshino umemi <umemi.0401@gmail.com>
2025-04-15 19:00:05 +0900

    no message

--- develop/structure03.php
+++ develop/structure03.php
@@ -37,102 +37,182 @@
 
   <section class="sections sec01 clearfix">
     <div class="wrap">
-      <div class="tit">防災対策</div>
+      <div class="tit">Safety</div>
       <ul class="lists list00">
-        <li class="full">
-          <p class="tit-img"><span>地震時に最寄階に自動停止する</span>エレベーター安全装置</p>
-          <p class="lead">エレベーター運転中に、地震管制装置が一定値を超えた地震の初期微動（P波）・主要動（S波）を感知すると、最寄階に速やかに停止します。また、停電した際には一旦停止後、停電時自動着床装置により最寄階に自動停止し、さらに天井の停電灯が点灯してエレベーター内を照らす他、インターホンが使用できるので外部との連絡も可能です。<br><span>※エレベーター運転中に急速な大きい地震により主要動（S波）［高］を感知した場合は、その瞬間に直ちに休止し、最寄階への移動をしない場合や扉が開かない場合がございます。</span></p>
+        <li>
+        <p class="tit-img"><span>地震時に最寄階に自動停止する</span>エレベーター安全装置</p>
+        <p class="lead">エレベーター運転中に、地震管制装置が一定値を超えた地震の初期微動（Ｐ波）・主要動（S波）を感知すると最寄階に速やかに停止します。また、停電した際には一旦停止後、停電時自動着床装置により最寄階に自動停止し、さらに天井の停電灯が点灯してエレベーター内を照らす他、インターホンが使用できるので外部との連絡も可能です。<br><span>※エレベーター運転中に急速な大きい地震により主要動（S波）［高］を感知した場合は、その瞬間に直ちに休止し、最寄階への移動をしない場合や扉が開かない場合がございます。<br>※非常用エレベーターは停電時、一旦停止後、非常用発電機が作動し通常の運転に戻ります。</span></p>
           <div class="image img01">
             <img src="./images/structure03/img01.png" alt="" class="">
-            <p class="annotation gothic bt">（概念図）</p>
-            <!-- <p class="mini">初期微動を感知することにより、より迅速な対応を可能とします。非常用エレベーターのみとなります。</p> -->
+            <p class="annotation gothic bt">（概念図）</span></p>
+          </div>
+          <p class="mini">※仮復旧運転は非常用エレベーター・各乗用エレベーターのみとなります。</p>
+        </li>
+        <li>
+          <p class="tit-img"><span>地震などの非常時のために</span>防災備品</p>
+          <p class="lead">地震などの非常時に備え、防災備品をご用意しました。初期共助に使用する救助用工具セット、 救助用ロープ、 スコップ、 防塵マスクなどを備えています。また、災害時のライフサポート備品として救急箱、 携帯電話充電・懐中電灯機能付き手回し充電ラジオ、カセットガス発電機、 非常用ロウソク、 飲料水用の折りたたみ式給水ポリタンク、 災害保存用飲料水、 災害用簡易トイレなども備えています。</p>
+          <div class="image">
+            <img src="./images/structure03/img02.png" alt="" class="">
+            <p class="annotation gothic bt">参考写真</p>
+            <p class="annotation annotation02 gothic bt">参考写真</p>
           </div>
         </li>
       </ul>
 
+
       <ul class="lists list01">
-        <li class="half">
-          <p class="tit-img"><span>給水ポンプが停電で停止しても水を使える</span>災害時の給水対策</p>
-          <p class="lead">1階共用部の共用トイレ、共用水栓（1階屋外散水栓および水栓柱）へは水道本管から圧力がかかっていれば、万一の停電で給水ポンプが停止しても、水を使うことができます。<span>※被災の状況により水が使えない場合があります。</span></p>
-          <p class="tit-img"><span>災害時などの避難経路に配慮した</span>オートドアガラス</p>
-          <p class="lead">災害時などに安全に避難できるようエントランス（サブエントランス、駐輪場）のオートドアには強化ガラスを採用しました。強化ガラスは衝撃に強く割れにくい上に、万一割れた場合でも破片の角が鋭利にならないという特徴があります。</p>
+      <li>
+          <p class="tit-img"><span>家具の転倒を予防するための</span>家具固定用下地補強</p>
+          <p class="lead">万一の地震時による家具転倒予防として、キッチンの一部の間仕切壁などに家具固定用下地補強を施しました。壁面の一部分にある下地補強に家具を固定することで家具が倒れにくくなり、住まう方の安全に配慮された仕様です。<br><span>※取付金具は別途お客様のご負担となります。</span></p>
+          <div class="image tb">
+            <img src="./images/structure03/img03.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <p class="tit-img"><span>戸外への避難路を確保する</span>対震ドア枠</p>
+          <p class="lead">万一の地震時に、玄関扉の枠が歪んでも、枠と扉の隙間を大きく設けることにより、扉の開放を容易にできるように配慮した対震ドア枠を採用しました。</p>
+          <div class="image tb">
+            <img src="./images/structure03/img04.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <p class="tit-img"><span>ベンチを災害時に炊き出しかまどとして使える</span>ベンチ兼用非常用炊き出しかまど</p>
+          <p class="lead">災害時には腰掛け板を外すと、非常用の「炊き出しかまど」となるベンチを設置しています。普段は敷地内にベンチとして設置しているため、置き場所にも困らないスマートな災害対策備品です。</p>
+          <div class="image tb">
+            <img src="./images/structure03/img05.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image pc">
+            <img src="./images/structure03/img03.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image pc">
+            <img src="./images/structure03/img04.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image pc">
+            <img src="./images/structure03/img05.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
           <p class="tit-img"><span>各住戸にお渡しする</span>防災リュック</p>
           <p class="lead">地震などの非常時に役立つグッズをまとめた防災リュックをお引渡し時に各住戸へお渡しします。<span>※飲料水や食品等は含まれておりません。詳細は係員にお尋ねください。</span></p>
         </li>
-        <li class="half">
-          <p class="tit-img"><span>地震などの非常時のために</span>防災備品</p>
-          <p class="lead">地震などの非常時に備え、防災備品をご用意しました。初期共助に使用する小型拡声器、救助用工具セット、救助用ロープ、スコップ、防塵マスクなどを備えています。また、災害時のライフサポート備品として救急箱、携帯電話充電・懐中電灯機能付き手回し充電ラジオ、非常用ロウソクなども備えています。</p>
-          <div class="image">
-            <img src="./images/structure03/img02.png" alt="" class="">
-            <p class="annotation gothic">参考写真2</p><p class="annotation right01 gothic">参考写真2</p>
-          </div>
+        <li>
+          <p class="tit-img"><span>地震の揺れで収納物が落下しないようにする</span>耐震ラッチ</p>
+          <p class="lead">キッチンの吊戸棚、洗面化粧台の三面鏡収納の扉には、地震の揺れによって扉が開いて、収納物が落下しないように、耐震ラッチを設置。住まう方の安全に配慮しています。さらに棚板が外れないように棚板脱落防止用ツメで棚板をしっかり固定しています。</p>
+          <!-- <div class="image">
+            <img src="./images/structure03/img06.png" alt="" class="">
+            <p class="annotation gothic"></p>
+          </div> -->
+        </li>
+        <li>
+          <p class="tit-img"><span>バルコニーからの避難も可能にする</span>バルコニー避難ハッチ</p>
+          <p class="lead">災害発生時、バルコニーを通って下階に降りられるよう各階バルコニーの必要箇所に避難ハッチを設けました。</p>
+        </li>
+        <li>
+          <p class="tit-img"><span>災害時の電力供給に対応する</span>非常用発電機</p>
+          <p class="lead">地震などの災害時に万一停電しても、共用部の一部設備［非常用エレベーター、共用部照明の一部、 共用部オートドアの一部、 住戸への給水など］を約48時間稼動させるため、非常用発電機から電力を供給します。（法規制2時間含む）<span>※被災の状況により電力を供給できない場合があります。<br>※稼動時間は燃料満載時前提です。また使用状況により稼動時間が変わる場合があります。<br>※火災信号が入った場合には、法的な防災設備の稼動が優先されます。</span></p>
+        </li>
+        <li>
+          <p class="tit-img"><span>煙の侵入を低減して避難できる</span>特別避難階段</p>
+          <p class="lead">万一の災害時の避難路として特別避難階段を設置。その階段には扉付きの前室（付室）を設けて火災時の煙の侵入を低減しています。</p>
+        </li>
+        <li>
+          <p class="tit-img"><span>熱を感知すると作動する</span>スプリンクラー</p>
+          <p class="lead">10階以上の住戸には、熱を感知すると自動的に散水する、スプリンクラーを設置し、迅速な初期消火への対応を実現します。</p>
+          <!-- <div class="image">
+            <img src="./images/structure03/img07.png" alt="" class="">
+            <p class="annotation gothic"></p>
+          </div> -->
+        </li>
+        <li>
+          <p class="tit-img"><span>停電時に自動点灯する</span>ホーム保安灯</p>
+          <p class="lead">停電時にライトが自動点灯するホーム保安灯を採用しました。コンセントから簡単に取り外せるので、非常時の携帯電灯としても使用できます。さらに、周囲が暗くなると自動点灯するナイトライト機能も付いています。</p>
+        </li>
+        <li>
+          <p class="tit-img"><span>給水ポンプが停電で停止しても水を使える</span>災害時の給水対策</p>
+          <p class="lead">1階共用部の共用水栓（外構散水栓、1階ゴミ置場）へは水道本管から給水ポンプを経由せずに給水しているので、万一の停電で給水ポンプが停止しても、水を使うことができます。<span>※被災の状況により水が使えない場合があります。</span></p>
+        </li>
+        <li>
+          <p class="tit-img"><span>火災の拡大を防ぐ</span>消火器</p>
+          <p class="lead">火災に備え、消火器が住戸内に設置されています。</p>
+        </li>
+        <li>
+          <p class="tit-img"><span>火災の拡大を防ぐ</span>屋内消火栓</p>
+          <p class="lead">火災が発生した時、初期消火を行うことができる屋内消火栓が共用部の各所に設置されています。<br><span>※スプリンクラー設置階は除く。</span></p>
+        </li>
+        <li>
+          <p class="tit-img"><span>火災を自動で知らせる</span>自動火災報知設備</p>
+          <p class="lead">火災が発生した際、周囲に知らせるための自動火災報知設備が共用部・専有部の各所に設置されています。<span>※火災によっては自動火災報知設備が作動しない場合があります。<br>※9階以下の住戸内には火災感知器が設置され、10階以上の住戸内にはスプリンクラーが設置されております。</span></p>
+        </li>
+        <li>
+          <p class="tit-img"><span>安全に避難するための</span>非常用照明</p>
+          <p class="lead">避難経路の一部には非常用照明を設置。停電時に自動点灯し、より安全に避難できます。</p>
         </li>
       </ul>
 
       <ul class="lists list02">
         <li>
           <p class="tit-img"><span>火災を警報と自動通報で知らせる</span>火災感知器</p>
-          <p class="lead">万一火災などが発生した場合は、火災感知器が作動。住戸内インターホン（親機）と玄関前インターホン（子機）より警報が鳴り、室内外に異常をお知らせするとともに、警備会社、住友不動産建物サービス設備管理センターへ自動通報されます。</p>
+          <p class="lead">万一火災などが発生した場合は、火災感知器が作動。住戸内インターホン（親機、副親機）と玄関前インターホン（子機）より警報が鳴り、室内外に異常をお知らせするとともに、警備会社、住友不動産建物サービス設備管理センターへ自動通報されます。</p>
           <div class="image tb">
-            <img src="./images/structure03/img03.png" alt="" class="">
-            <p class="annotation gothic">参考写真2</p>
+            <img src="./images/structure03/img08.png" alt="" class="">
+            <p class="annotation gothic">参考写真</p>
           </div>
         </li>
         <li>
-          <p class="tit-img"><span>地震の揺れで収納物が落下しないようにする</span>耐震ラッチ</p>
-          <p class="lead">洗面化粧台の三面鏡収納の扉には、地震の揺れによって扉が開いて、収納物が落下しないように、耐震ラッチを設置。住まう方の安全に配慮しています。</p>
+          <p class="tit-img"><span>ガス漏れを警報と自動通報で知らせる</span>ガス漏れ検知器</p>
+          <p class="lead">万一ガス漏れが発生した場合は、ガス漏れ検知器が作動。当該住戸内のインターホン（親機および副親機）と玄関前インターホン（子機）より警報音が鳴り、室内外に異常をお知らせするとともに、警備会社および住友不動産建物サービス設備管理センターへ自動通報されます。</p>
           <div class="image tb">
-            <img src="./images/structure03/img04.png" alt="" class="">
-            <p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img09.png" alt="" class="">
+            <p class="annotation gothic">参考写真</p>
           </div>
         </li>
         <li>
-          <p class="tit-img"><span>バルコニーからの避難も可能にする</span>バルコニー避難ハッチ</p>
-          <p class="lead">災害発生時、バルコニーを通って下階に降りられるよう各階バルコニーの必要箇所に避難ハッチを設けました。避難ハッチのない住戸は、バルコニーの隔板を破って同一階で移動することにより、避難ハッチを利用することができます。</p>
-          <p class="tit-img"><span>停電時に自動点灯する</span>ホーム保安灯</p>
-          <p class="lead">停電時にライトが自動点灯するホーム保安灯を採用しました。コンセントから簡単に取り外せるので、非常時の携帯電灯としても使用できます。さらに、周囲が暗くなると自動点灯するナイトライト機能も付いています。</p>
+          <p class="tit-img"><span>災害時に役立つ</span>非常用マンホールトイレ</p>
+          <p class="lead">災害時に敷地内の特定のマンホールのフタを外して簡易トイレ（非常用マンホールトイレ）を設置するだけですぐに利用できます。</p>
+          <p class="tit-img"><span>ガスの立ち消えに対応する</span>ガスコンロ立ち消え安全装置</p>
+          <p class="lead">コンロ・魚焼きグリルには、立ち消え安全装置が付いています。火が消えるとガスが自動遮断されます。</p>
         </li>
         <li>
           <div class="image pc">
-            <img src="./images/structure03/img03.png" alt="" class="">
-            <p class="annotation gothic">参考写真2</p>
+            <img src="./images/structure03/img08.png" alt="" class="">
+            <p class="annotation gothic">参考写真</p>
           </div>
         </li>
         <li>
           <div class="image pc">
-            <img src="./images/structure03/img04.png" alt="" class="">
-            <p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img09.png" alt="" class="">
+            <p class="annotation gothic">参考写真</p>
           </div>
         </li>
         </ul>
 
       <ul class="lists list03">
-        <li class="half">
-          <p class="tit-img"><span>戸外への避難路を確保する</span>対震ドア枠</p>
-          <p class="lead">万一の地震時に、玄関扉の枠が歪んでも、枠と扉の隙間を大きく設けることにより、扉の開放を容易にできるように配慮した対震ドア枠を採用しました。</p>
-          <div class="image tb">
-            <img src="./images/structure03/img05.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
+      <li>
+          <p class="tit-img"><span>ガスの消し忘れに対応する</span>ガスコンロ消し忘れタイマー</p>
+          <p class="lead">コンロ・魚焼きグリルには、それぞれ一定時間で自動的にガスを止め、ブザーでお知らせする安全装置が付いています。</p>
+          <div class="image">
+            <img src="./images/structure03/img10.png" alt="" class="">
+            <p class="annotation gothic"></p>
           </div>
         </li>
-        <li class="half">
-          <p class="tit-img"><span>家具の転倒を予防するための</span>家具固定用下地補強</p>
-          <p class="lead">万一の地震時による家具転倒予防として、キッチンの一部の間仕切壁などに家具固定用下地補強を施しました。壁面の一部分にある下地補強に家具を固定することで家具が倒れにくくなり、住まう方の安全に配慮された仕様です。<span>※住戸タイプにより設置箇所は異なります。詳細は係員にお尋ねください。<br>※取付金具は別途お客様のご負担となります。</span></p>
-          <div class="image tb">
-            <img src="./images/structure03/img06.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p><p class="annotation right01 gothic">（概念図）</p>
-          </div>
-        </li>
-        <li class="half">
-          <div class="image pc">
-            <img src="./images/structure03/img05.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li class="half">
-          <div class="image pc">
-            <img src="./images/structure03/img06.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p><p class="annotation right01 gothic">（概念図）</p>
+        <li>
+          <p class="tit-img"><span>温度が上がりすぎるのを防ぐ</span>天ぷら油過熱防止機能</p>
+          <p class="lead">コンロには天ぷら油過熱防止装置が付いています。油が一定温度にまで上昇すると、自動的にガスを止めます。</p>
+          <div class="image">
+            <img src="./images/structure03/img11.png" alt="" class="">
+            <p class="annotation gothic"></p>
           </div>
         </li>
       </ul>
@@ -143,7 +223,7 @@
 
 
   <div class="annotation-area">
-  <p class="page-annotation js-scroll wrap clearfix">※掲載の参考写真1は本物件と同様仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれておりません。<br>※掲載の参考写真2はメーカーカタログ等の参考写真であり本物件のものではありません。※設備・仕様はタイプにより異なります。</p></div>
+  <p class="page-annotation js-scroll wrap clearfix"></p></div>
 </main>
 
 <div class="structure-link structure-link--3">
